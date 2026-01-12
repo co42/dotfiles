0a21e8e4bb30aac3c9dc8ad62e6b4da0356a7b2b
Corentin REGAL <corentin@huggingface.co>
2026-01-12 18:34:40 +0100

    Reorganize dotfiles: add configs, remove nvim
    - Add .gitignore to manage bare repo properly
    - Add k9s config and catppuccin skins
    - Add starship prompt config
    - Add ghostty terminal config
    - Update alacritty config
    - Remove nvim config (will re-add when stable)

--- .config/nvim/lua/co/plugins/crates.lua
+++
@@ -1,8 +0,0 @@
--- Help manage crates.io dependencies
--- https://github.com/saecki/crates.nvim
-return {
-  'saecki/crates.nvim',
-  tag = 'stable',
-  event = { 'BufRead Cargo.toml' },
-  opts = {},
-}
