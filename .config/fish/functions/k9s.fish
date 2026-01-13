d9629c5d0f44d3f642e4780ddcc8179d46422dad
Corentin REGAL <corentin@huggingface.co>
2026-01-13 09:50:40 +0100

    fish: fix k9s function

--- .config/fish/functions/k9s.fish
+++ .config/fish/functions/k9s.fish
@@ -1,5 +1,6 @@
 function k9s --wraps=k9s --description 'Launch k9s with theme based on macOS appearance'
-    if test (defaults read -g AppleInterfaceStyle 2>/dev/null) = "Dark"
+    set -l appearance (defaults read -g AppleInterfaceStyle 2>/dev/null)
+    if test "$appearance" = "Dark"
         sed -i '' 's/skin:.*/skin: catppuccin-macchiato-transparent/' ~/.config/k9s/config.yaml
     else
         sed -i '' 's/skin:.*/skin: catppuccin-latte-transparent/' ~/.config/k9s/config.yaml
